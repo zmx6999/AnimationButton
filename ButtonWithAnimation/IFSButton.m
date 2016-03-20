84f27dfd960f3472989f56b3682d98a83a80d9ae
zmx6999 <2455847459@qq.com>
2016-03-19 23:48:33 -0700

    second commit

--- ButtonWithAnimation/IFSButton.m
+++ ButtonWithAnimation/IFSButton.m
@@ -9,20 +9,20 @@
 #import "IFSButton.h"
 #import "MMMaterialDesignSpinner.h"
 
+#define originCornerR 3
 #define scale 1.2
-#define originCornerRadius 3
 
-#define w (self.frame.size.width)
-#define h (self.frame.size.height)
-#define btnW (w / scale)
-#define btnH (h / scale)
+#define viewW (self.frame.size.width)
+#define viewH (self.frame.size.height)
+#define btnW (viewW / scale)
+#define btnH (viewH / scale)
 
 @interface IFSButton ()
 
-@property (weak, nonatomic) UIButton *button;
+@property (nonatomic, weak) UIButton *button;
 @property (nonatomic, weak) UIView *bgView;
 @property (nonatomic, weak) MMMaterialDesignSpinner *spinner;
-@property (nonatomic, weak) UILabel *textLabel;
+@property (nonatomic, weak) UILabel *label;
 
 @property (nonatomic, strong) NSTimer *timer;
 
@@ -34,56 +34,56 @@
     [super awakeFromNib];
     
     UIButton *button = [[UIButton alloc] init];
+    [self addSubview:button];
     self.button = button;
-    [self addSubview:self.button];
-    [self.button addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+    [button addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
     
     UIView *bgView = [[UIView alloc] init];
+    [self.button addSubview:bgView];
     self.bgView = bgView;
-    self.bgView.backgroundColor = [UIColor colorWithRed:1.0 green:0.7 blue:0.3 alpha:1.0];
-    [self.button addSubview:self.bgView];
-    self.bgView.layer.cornerRadius = originCornerRadius;
-    self.bgView.userInteractionEnabled = NO;
+    bgView.backgroundColor = [UIColor greenColor];
+    bgView.layer.cornerRadius = originCornerR;
+    bgView.userInteractionEnabled = NO;
     
     MMMaterialDesignSpinner *spinner = [[MMMaterialDesignSpinner alloc] init];
+    [self.button addSubview:spinner];
     self.spinner = spinner;
-    [self.button addSubview:self.spinner];
-    self.spinner.lineWidth = 2;
-    self.spinner.tintColor = [UIColor whiteColor];
-    self.spinner.hidesWhenStopped = YES;
+    spinner.lineWidth = 2;
+    spinner.tintColor = [UIColor whiteColor];
+    spinner.hidesWhenStopped = YES;
     
-    UILabel *textLabel = [[UILabel alloc] init];
-    self.textLabel = textLabel;
-    [self.button addSubview:self.textLabel];
-    self.textLabel.text = @"发送验证码";
-    self.textLabel.textColor = [UIColor whiteColor];
-    self.textLabel.font = [UIFont systemFontOfSize:15];
-    self.textLabel.textAlignment = NSTextAlignmentCenter;
+    UILabel *label = [[UILabel alloc] init];
+    [self.button addSubview:label];
+    self.label = label;
+    self.label.text = @"发送验证码";
+    self.label.textColor = [UIColor whiteColor];
+    self.label.font = [UIFont systemFontOfSize:15];
+    self.label.textAlignment = NSTextAlignmentCenter;
 }
 
-- (void)onClick {
-    self.button.userInteractionEnabled = NO;
+- (void)onClick:(UIButton *)sender {
+    sender.userInteractionEnabled = NO;
     [self startSend];
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     
+    self.button.center = CGPointMake(viewW * 0.5, viewH * 0.5);
     self.button.bounds = CGRectMake(0, 0, btnW, btnH);
-    self.button.center = CGPointMake(w * 0.5, h * 0.5);
     
     self.bgView.frame = self.button.bounds;
     
-    self.spinner.bounds = CGRectMake(0, 0, btnH, btnH);
     self.spinner.center = CGPointMake(btnW * 0.5, btnH * 0.5);
+    self.spinner.bounds = CGRectMake(0, 0, btnH, btnH);
     
-    self.textLabel.frame = self.button.bounds;
+    self.label.frame = self.button.bounds;
 }
 
 - (void)startSend {
     CABasicAnimation *anim = [[CABasicAnimation alloc] init];
     anim.keyPath = @"cornerRadius";
-    anim.fromValue = @(originCornerRadius);
+    anim.fromValue = @(originCornerR);
     anim.toValue = @(btnH * scale * 0.5);
     anim.duration = 0.3;
     [self.bgView.layer addAnimation:anim forKey:nil];
@@ -92,17 +92,17 @@
     [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:6 options:UIViewAnimationOptionCurveLinear animations:^{
         self.bgView.layer.bounds = CGRectMake(0, 0, btnW * scale, btnH * scale);
     } completion:^(BOOL finished) {
+        self.label.text = nil;
         [UIView animateWithDuration:0.3 animations:^{
-            self.textLabel.text = nil;
             self.bgView.layer.bounds = CGRectMake(0, 0, btnH * scale, btnH * scale);
         } completion:^(BOOL finished) {
+            [self setupTimer];
             [self.spinner startAnimating];
-            [self addTimer];
         }];
     }];
 }
 
-- (void)addTimer {
+- (void)setupTimer {
     self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
     [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
     [self.timer fire];
@@ -112,11 +112,11 @@
     static int n = 20;
     
     if (n > 0) {
-        self.textLabel.text = [NSString stringWithFormat:@"%d", n--];
+        self.label.text = [NSString stringWithFormat:@"%d", n--];
     } else {
-        self.textLabel.text = nil;
-        [self.spinner stopAnimating];
+        self.label.text = nil;
         [self removeTimer];
+        [self.spinner stopAnimating];
         [self endSend];
         n = 20;
     }
@@ -131,12 +131,12 @@
     [UIView animateWithDuration:0.3 animations:^{
         self.bgView.layer.bounds = CGRectMake(0, 0, btnW * scale, btnH * scale);
     } completion:^(BOOL finished) {
-        self.textLabel.text = @"发送验证码";
+        self.label.text = @"发送验证码";
         
         CABasicAnimation *anim = [[CABasicAnimation alloc] init];
         anim.keyPath = @"cornerRadius";
         anim.fromValue = @(btnH * scale * 0.5);
-        anim.toValue = @(originCornerRadius);
+        anim.toValue = @(originCornerR);
         anim.duration = 0.3;
         [self.bgView.layer addAnimation:anim forKey:nil];
         self.bgView.layer.cornerRadius = [anim.toValue doubleValue];
